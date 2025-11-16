589c08cf11d0bbd290da051fe64324b3f1601188
ywc43 <wenchenyang279@gmail.com>
2025-11-16 02:46:29 -0500

    admin postimages

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 from messaging.models import Message
-from .models import Post
+from .models import Post, PostImages
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Message)
 admin.site.register(Post)
+admin.site.register(PostImages)
